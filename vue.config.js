3af46aef3d50f9ca2d404bc4a205983aa5044690
ridger <ridger@live.cn>
2025-05-13 10:28:58 -0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
+// vue.config.js
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  pwa: {
+  // ...其他配置
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sosconf2020/' // 将 <REPONAME> 替换为您的 GitHub 仓库名称
+    : '/',
+  transpileDependencies: ["vuetify"], // 从您上传的文件中保留
+  pwa: { // 从您上传的文件中保留
     name: "SOSCONF.zh 2020",
     themeColor: "#589bad"
   }
